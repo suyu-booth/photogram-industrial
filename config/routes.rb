cc6c62fed65d2aa82843297ab1cbd2bce308d31e
suyu-booth <suyu@chicagobooth.edu>
2025-04-02 22:21:46 +0000

    step31

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "photos#index"
   resources :likes
   resources :follow_requests
   resources :comments
@@ -15,5 +16,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "photos#index"
+  
 end
