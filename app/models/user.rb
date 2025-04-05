39f21335d5891ea5440c1720bb83e934e25560c9
suyu-booth <suyu@chicagobooth.edu>
2025-04-05 20:58:15 +0000

    completion

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,8 @@ class User < ApplicationRecord
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :pending_sent_follow_requests, -> { pending }, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :pending_received_follow_requests, -> { pending }, foreign_key: :recipient_id, class_name: "FollowRequest"
 
   has_many :likes, foreign_key: :fan_id
 
@@ -48,6 +50,7 @@ class User < ApplicationRecord
   has_many :liked_photos, through: :likes, source: :photo
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
   has_many :followers, through: :accepted_received_follow_requests, source: :sender
+  has_many :pending, through: :pending_sent_follow_requests, source: :recipient
 
   has_many :feed, through: :leaders, source: :own_photos
   has_many :discover, -> { distinct }, through: :leaders, source: :liked_photos
