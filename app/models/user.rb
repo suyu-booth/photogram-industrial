4825fe87675d8885f56fa8c6583fcf72bb093f86
suyu-booth <suyu@chicagobooth.edu>
2025-04-05 03:35:27 +0000

    some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,9 +36,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: "author_id"
 
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
-  has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
-  has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
 
   has_many :likes, foreign_key: :fan_id
 
